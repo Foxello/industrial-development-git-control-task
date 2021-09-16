8dd2b47f9d89ba5044fdea45e64fd2e25270aeec
Andrey <a.bolotov-2@yandex.ru>
2021-09-16 14:34:53 +0300

    Resolve conflict (cherry-pick)

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,4 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, world!1");
-        System.out.prinln("New Feature!")
-    }
+        System.out.println("Hello, world!1\n");
 }
